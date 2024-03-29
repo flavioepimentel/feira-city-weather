733444c6fca3e948da21e14fdefa0cbb39370c1c
Flávio E Pimentel <63177834+flavioepimentel@users.noreply.github.com>
2024-03-29 19:26:30 -0300

    feat :tada: Primeiro commit

---
+++ src/request_weather.py
@@ -0,0 +1,46 @@
+import openmeteo_requests
+
+import requests_cache
+import pandas as pd
+from retry_requests import retry
+
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
+url = "https://api.open-meteo.com/v1/forecast"
+params = {
+	"latitude": -12.243134638899027,
+	"longitude": -38.96115471090046,
+	"daily": ["temperature_2m_max", "rain_sum"],
+	"start_date": "2016-01-01",
+	"end_date": "2024-01-01"
+}
+responses = openmeteo.weather_api(url, params=params)
+
+# Process first location. Add a for-loop for multiple locations or weather models
+response = responses[0]
+print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+print(f"Elevation {response.Elevation()} m asl")
+print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+# Process daily data. The order of variables needs to be the same as requested.
+daily = response.Daily()
+daily_temperature_2m_max = daily.Variables(0).ValuesAsNumpy()
+daily_rain_sum = daily.Variables(1).ValuesAsNumpy()
+
+daily_data = {"date": pd.date_range(
+	start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
+	end = pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
+	freq = pd.Timedelta(seconds = daily.Interval()),
+	inclusive = "left"
+)}
+daily_data["temperature_2m_max"] = daily_temperature_2m_max
+daily_data["rain_sum"] = daily_rain_sum
+
+daily_dataframe = pd.DataFrame(data = daily_data)
+print(daily_dataframe)
